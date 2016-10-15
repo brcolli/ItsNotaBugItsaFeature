f042df6e083177fe91051a9fcf52077fe3cb4ed8
Ben Collins <sstben@gmail.com>
2016-10-15 16:22:39 -0500

    movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField] private GameObject Player;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    gameObject.transform.position = Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = Player.transform.position; // Update to player's position
+
+
+    }
+}
